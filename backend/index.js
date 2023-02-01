86d00356f890bdfb56b59447f5194add9062e5c8
ke444a <kechaofficial@gmail.com>
2023-02-01 23:29:01 +0000

    Changed CORS options

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://film-fiesta-movie-app.onrender.com',
     methods: ['GET'],
 }
 app.use(cors(corsOptions));
